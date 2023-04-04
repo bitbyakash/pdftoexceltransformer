7d80b87b2333c2ad9d0b27749cc7e69b2b53bce9
unknown <diskligma@gmail.com>
2023-04-03 22:55:29 -0400

    update code

--- pdfapp.py
+++ pdfapp.py
@@ -11,19 +11,11 @@ from xlsxwriter import Workbook
 
 st.set_page_config(page_title="Cyberakash's PDF to Excel", page_icon=":robot_face:", layout="wide") 
 
-#subprocess.run(["choco", "upgrade", "all"])
-#subprocess.run(["choco", "install", "ghostscript"])
-
-
-#subprocess.run(["apt-get", "update"])
-#subprocess.run(["apt-get", "install", "-y", "ghostscript"])
-
-
-@st.cache_data
-def gh():
-    proc = subprocess.Popen('apt-get install -y ghostscript', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
-    proc.wait()
-gh()
+#@st.cache_data
+#def gh():
+#    proc = subprocess.Popen('apt-get install -y ghostscript', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
+#    proc.wait()
+#gh()
 
 # Use local CSS
 def local_css(file_name):
@@ -38,7 +30,7 @@ st.title("Akash's PDF to Excel Transformer :robot_face:")
 st.write("---")
 
 # upload the pdf
-st.write('NOTE - This app only converts data tables from text-based PDFs and not scanned documents.')
+st.write('NOTE  -  This app only converts data tables from text-based PDFs and not scanned documents.')
 
 with st.container():
     st.write('##')
@@ -51,25 +43,39 @@ with st.container():
 st.write("---")
 
 # enter page number
-page_number = st.text_input("Enter the page number from PDF file which you want converted to Excel, for eg: 3", value=1)
+with st.container():
+    left_column, right_column = st.columns((1, 1))
+    with left_column:
+        st.markdown("### Fine Tuning")
+        page_number = st.text_input(""" From the PDF file, enter the page number that you want converted to Excel, for e.g., 3 
+                                    """, value=1)
+    with right_column:
+        st.empty()
 
+#PDF conversion
 if input_pdf is not None:
     with open("input.pdf", "wb") as f:
         base64_pdf = base64.b64encode(input_pdf.read()).decode('utf-8')
         f.write(base64.b64decode(base64_pdf))
     
-    table = cam.read_pdf("input.pdf", pages=page_number, flavor='stream')
+    table = cam.read_pdf("input.pdf", pages=page_number, flavor='lattice')
 
-    st.markdown("### Number of Tables")
-
-    st.write(table)
+    #st.markdown("### Number of Tables")
+    #st.write(table)
 
     if len(table) > 0:
         # select table to convert to Excel
-        option = st.selectbox(label="Select the table you want to convert to Excel", options=range(len(table)))
+        with st.container():
+            left_column, right_column = st.columns((1, 1))
+            with left_column:
+                option = st.selectbox(label="Select the table from above selected page that you want converted to Excel", options=range(len(table)))
+            with right_column:
+                st.empty()
+
         output_table = table[option].df
 
-        st.markdown('### PDF Table')
+        st.write("---")
+        st.markdown('### Table Preview')
 
         # display the selected table without index column and row
         #st.write(output_table)
@@ -83,9 +89,14 @@ if input_pdf is not None:
         output.seek(0)
 
         # Download
+        st.write("---")
         st.write('##')
         st.download_button(
             label="Download Selected Table from PDF File",
             data=output.getvalue(),
             file_name=f"Table {option} from PDF file.xlsx"
         )
+    else:
+        st.write('##')
+        st.write('##')
+        st.markdown('### No table found on uploaded PDF file')
